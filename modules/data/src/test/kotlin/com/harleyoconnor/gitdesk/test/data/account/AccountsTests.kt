9089e71d749ee095a63f62bd3306ad919ae0a1b7
Harleyoc1 <harley.oconnor@icloud.com>
2022-01-27 17:39:39 +0000

    Fix account test causing CI to fail

--- modules/data/src/test/kotlin/com/harleyoconnor/gitdesk/test/data/account/AccountsTests.kt
+++ modules/data/src/test/kotlin/com/harleyoconnor/gitdesk/test/data/account/AccountsTests.kt
@@ -36,10 +36,10 @@ class AccountsTests {
     fun `test getting user details with invalid session key fails`() {
         // Actual session keys are 128 chars long, so this should always fail.
         val response = getAccountRequest(Session("cvfdfdcc")).get(30, TimeUnit.SECONDS)
-        assert(response.wasSuccess() || response.getCode() == 403) {
+        assert(response.wasSuccess() || response.getCode() == 400) {
             "Server error executing request. HTTP Response Code: " + response.getCode().toString()
         }
-        Assertions.assertTrue(response.getCode() == 403)
+        Assertions.assertTrue(response.getCode() == 400)
     }
 
     @EnabledIf("com.harleyoconnor.gitdesk.test.data.account.AccountsTests#isSessionKeySet", disabledReason = "No session key property set.")
